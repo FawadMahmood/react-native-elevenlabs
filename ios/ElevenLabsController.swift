085185574529e2713480d0073ae1447233c918db
Fawad Mahmood <50448569+FawadMahmood@users.noreply.github.com>
2025-06-12 19:14:51 +0500

    chore: remove disconnect onError

--- ios/ElevenLabsController.swift
+++ ios/ElevenLabsController.swift
@@ -53,9 +53,6 @@ public class ElevenLabsController: NSObject {
     }
     callbacks.onError = { [weak self] errorMessage, info in
         self?.handleEvent(ElevenLabsEventEvents.onError, ["error": errorMessage, "info": info ?? ""])
-        self?.conversation?.endSession()
-        self?.conversation = nil
-        self?.handleEvent(ElevenLabsEventEvents.onDisconnect, [:])
     }
     callbacks.onStatusChange = { [weak self] newStatus in
         self?.updateStatus(newStatus)
@@ -66,6 +63,7 @@ public class ElevenLabsController: NSObject {
     callbacks.onVolumeUpdate = { [weak self] newVolume in
         self?.handleEvent(ElevenLabsEventEvents.onVolumeUpdate, ["volume": newVolume])
     }
+  
     return callbacks
   }
   
